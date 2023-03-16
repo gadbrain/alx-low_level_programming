fa012478d6fda774053a9edd016dc6bd1f80683f
gadbrain <gadbrain@gmail.com>
2023-03-16 03:34:19 -0700

    Updated other files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This program prints an output using the PUT keyword
+ *
+ * Created by Gabriel Olayinka
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This program prints output using the PRINTF keyword
+ *
+ * Created by Gabriel K
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
